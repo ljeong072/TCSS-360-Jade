ba67a5d92ef113375faa0b01b87532c71b746d82
ljeong2 <Peter@z235>
2024-04-02 19:04:33 -0700

    Sprint 0 Lucas' commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("THIS IS SPRINT 0, testing Reece");
+        System.out.println("THIS IS SPRINT 0, testing LUCAS");
     }
 }
