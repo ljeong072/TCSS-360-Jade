d06c22d03f6db3430351b15029115ca1ef5e00a8
CoffeGlory <aqiu131@outlook.com>
2024-04-02 19:17:37 -0700

    testting han

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("THIS IS SPRINT 0, testing LUCAS");
+        System.out.println("THIS IS SPRINT 0, testing hhh ");
     }
 }
