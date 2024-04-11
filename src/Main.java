8ef5d714f01f6f12a349ce0a17912fbc7de7e8e5
ljeong2 <Peter@z235>
2024-04-10 20:04:26 -0700

    Sprint 0 MEETING 3
    Finished the code for sprint 0

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javax.swing.;
 import java.awt.;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+/*
+ * @version 1.0
+ */
 
 public class Main {
     public static void main(String[] args) {
@@ -12,3 +15,16 @@ public class Main {
                 startGUI();
             }
         });
+    }
+    private static void startGUI() {
+        JFrame frame = new JFrame("Sprint0");
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 50));
+        panel.setBackground(new Color(0,0,0));
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500); // change later depending on what we need to do
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Fully close the operation.
+    }
+}
